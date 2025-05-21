e582276899a3fcb2f3f40d328b7f61dc63bffba8
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-05-22 01:35:13 +0200

    update the experience years

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 3, suffix: "+", label: "Years of Experience" },
+  { value: 3, suffix: "+", label: "Satisfied Clients" },
+  { value: 5, suffix: "+", label: "Completed Projects" },
+  { value: 3, suffix: "%", label: "Client Retention Rate" },
 ];
 
 const logoIconsList = [
